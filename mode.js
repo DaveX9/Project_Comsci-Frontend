0670d752319e7778e068ee7b7d1c60ad6ca1d715
DaveX9 <sahadevthapa645462@gmail.com>
2024-08-21 00:29:07 +0700

    project

---
+++ mode.js
@@ -0,0 +1,9 @@
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        this.textContent = '☀️ Light Mode';
+    } else {
+        this.textContent = '🌙 Dark Mode';
+    }
+});
